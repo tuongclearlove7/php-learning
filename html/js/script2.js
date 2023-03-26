d13457cd65ef154299a4dc19297ff25bf8730dc1
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-03-26 23:00:27 +0700

    update

---
+++ html/js/script2.js
@@ -0,0 +1,177 @@
+function change1(){
+
+
+    document.querySelector('.change1').onclick = function(){
+
+        document.querySelector(".render-new").innerHTML = `
+        <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+        <div class="student">
+            <p>Sau khi hoàn thành khoá học lập trình viên chuyên nghiệp ở trung tâm em đã nắm kiến thức trọng tâm của ngôn ngữ Java. Nhờ sự hỗ trợ tìm việc làm của thầy Việt và Cô Lan em đã tìm được công việc phù hợp với bản thân ngay sau tốt nghiệp.</p>
+            <p>Em xin gửi lời cảm ơn chân thành và sâu sắc đến 2 thầy Trần Đức Linh, thầy Lê Đức Anh đã giảng dạy, support nhiệt tình trong suốt khoá học, và thầy Việt và cô Lan đã tư vấn, hỗ trợ việc làm cho em ạ.</p>
+            <p>Em xin chúc trung tâm ngày càng phát triển, tạo môi trường học tập tốt cho các bạn có đam mê với ngành phần mềm.</p>
+            
+            <div class="student2">
+                <img src="../image/banner3.jpeg" alt="">
+                <h3>Hồ Phương Thảo_LTVJAVA_CÔNG TY SDT</h3>
+            </div>`;
+    }
+
+    
+}
+
+ function change2(){
+        
+     document.querySelector('.change2').onclick = function(){
+
+        document.querySelector(".render-new").innerHTML = `
+        <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+        <div class="student">
+        <p>Cảm ơn, cảm ơn, cảm ơn iViettech và thầy Quý rất nhiều(Cái gì quan trọng phải nói 3 lần)
+        </p>
+        <p>Biết đến iViettech cũng thật tình cờ và cũng là sự có duyên được làm học sinh của thầy Quý . Nói vậy cũng bởi mình đã là một người đi làm nhưng luôn có đam mê nhiệt huyết với lập trình Android kể từ khi là sinh viên Đại Học, tuy nhiên nhờ có iViettech thì mình mới được làm việc đúng với đam mê của mình.</p>
+        <p>Chỉ vỏn vẹn 1 buổi chiều tìm kiếm lớp học Android và mình tìm thấy iViettech, quyết định liên hệ và đăng ký học ngay lập tức dù lớp đã khai giảng và học được gần một tháng.
+            Chỉ vỏn vẹn 4 tháng mình đã có thể hoàn thành một dự án Android xịn xò với sự giúp đỡ nhiệt tình của thầy Quý.
+            Chỉ vỏn vẹn 4 ngày sau đợt bảo vệ thì mình đã nhận được thư mời đi phỏng vấn và báo đậu và vài ngày nữa thôi mình chính thức nhận công việc mới.</p>
+        <p>Vậy mình mới nói việc cảm ơn trung tâm và thầy Quý là rất quan trọng.</p>
+      
+            <div class="student2">
+                <img src="../image/banner3.jpeg" alt="">
+                <h3>PHẠM HỒNG SƠN_ANDROID_RIKKEI</h3>
+            </div>`;
+    }
+
+
+ }
+
+ function change3(){
+        
+    document.querySelector('.change3').onclick = function(){
+
+       document.querySelector(".render-new").innerHTML = `
+       <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+       <div class="student">
+       <p>Lời đầu tiên em xin gửi lời cảm ơn đến quý thầy cô IViettech đã cất công truyền đạt kiến thức đến người học trái ngành và gần tiếp cận độ tuổi 3x như em.
+       </p>
+       <p>Quyết định chuyển ngành ở tuổi này cũng làm em suy nghĩ rất nhiều, nhưng nhờ có sự tư vấn và động viên của thầy cô, và với đam mê lập trình của mình, chỉ sau 8 tháng học khoá lập trình viên chuyên nghiệp JAVA, em đã nhận được cơ hội việc làm mong muốn.
+       Em xin gửi lời cảm ơn đến thầy Trần Đức Linh, thầy Lê Đức Anh đã giảng dạy, hỗ trợ em trong suốt khoá học, cảm ơn thầy Việt và cô Lan đã tư vấn, hỗ trợ việc làm cho em ạ.</p>
+       <p>Chỉ vỏn vẹn 1 buổi chiều tìm kiếm lớp học Android và mình tìm thấy iViettech, quyết định liên hệ và đăng ký học ngay lập tức dù lớp đã khai giảng và học được gần một tháng.
+          </p>
+       <p>Kính chúc quý thầy cô và trung tâm sẽ càng ngày càng phát triển và gặt hái nhiều thành công hơn!
+       Em xin chân thành cảm ơn!!</p>
+     
+           <div class="student2">
+               <img src="../image/banner3.jpeg" alt="">
+               <h3>HUỲNH TRỌNG HẬU_LẬP TRÌNH VIÊN CHUYÊN NGHIỆP JAVA_CÔNG TY FSOFT</h3>
+           </div>`;
+   }
+
+
+}
+
+
+function change4(){
+
+
+    document.querySelector('.change4').onclick = function(){
+
+        document.querySelector(".render-new").innerHTML = `
+        <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+        <div class="student">
+            <p>Mình đã tham gia khoá học Kiểm thử phần mềm tại đây. Nhờ sự giảng dạy rất nhiệt tình, sự tư vấn và hỗ trợ tìm việc làm sau khoá học rất tận tâm của các thầy cô ở trung tâm mà mình đã tìm được việc làm phù hợp. Xin chân thành cám ơn các thầy cô và mọi người ở trung tâm nhiều.</p>
+           
+            <div class="student2">
+                <img src="../image/banner3.jpeg" alt="">
+                <h3>ĐẶNG THỊ MI NA_TESTER_FSOFT</h3>
+            </div>`;
+    }
+
+    
+}
+
+ function change5(){
+        
+     document.querySelector('.change5').onclick = function(){
+
+        document.querySelector(".render-new").innerHTML = `
+        <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+        <div class="student">
+        <p>Đầu tiên em xin gửi lời cảm ơn sâu sắc đến thầy Hải, thầy Tuấn đã giảng dạy nhiệt tình trong thời gian em tham gia khoá học ở Iviettech.
+        </p>
+        <p>Em cảm ơn thầy Việt, cô Lan đã truyền đạt kinh nghiệm, củng cố lại kiến thức, hỗ trợ em trong quá trình tìm việc.
+        Thật sự may mắn khi em được biết đến iViettech, và cảm thấy rất vui khi mình đã chọn đúng nơi để giúp bản thân trao dồi thêm những kiến thức bổ ích. Trong thời gian tham gia khóa học, em hài lòng với mọi thứ tại đây, từ cơ sở vật chất, chương trình học, thái độ của các chị giáo vụ, sự hỗ trợ của cô Lan - thầy Việt, và đặt biệt là sự giảng dạy hết sức tận tâm của các thầy.</p>
+        
+        <p>Sau khi tốt nghiệp tại trung tâm, em đã có được một hành trang tốt để thực hiện những mục tiêu của mình trong tương lai.
+        Cuối cùng, em kính chúc quý thầy cô, anh chị thật nhiều sức khỏe, trung tâm ngày càng phát triển hơn nữa! ❤️</p>
+      
+            <div class="student2">
+                <img src="../image/banner3.jpeg" alt="">
+                <h3>NGUYỄN THỊ BÍCH NI – LẬP TRÌNH FRONTEND – GLOBAL DESIGN IT</h3>
+            </div>`;
+    }
+
+
+ }
+
+ function change6(){
+        
+    document.querySelector('.change6').onclick = function(){
+
+       document.querySelector(".render-new").innerHTML = `
+       <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+       <div class="student">
+       <p>Lời đầu tiên em xin gửi lời cảm ơn đến quý thầy cô IViettech đã cất công truyền đạt kiến thức đến người học trái ngành và gần tiếp cận độ tuổi 3x như em.
+       </p>
+       <p>Quyết định chuyển ngành ở tuổi này cũng làm em suy nghĩ rất nhiều, nhưng nhờ có sự tư vấn và động viên của thầy cô, và với đam mê lập trình của mình, chỉ sau 8 tháng học khoá lập trình viên chuyên nghiệp JAVA, em đã nhận được cơ hội việc làm mong muốn.
+       Em xin gửi lời cảm ơn đến thầy Trần Đức Linh, thầy Lê Đức Anh đã giảng dạy, hỗ trợ em trong suốt khoá học, cảm ơn thầy Việt và cô Lan đã tư vấn, hỗ trợ việc làm cho em ạ.</p>
+       <p>Chỉ vỏn vẹn 1 buổi chiều tìm kiếm lớp học Android và mình tìm thấy iViettech, quyết định liên hệ và đăng ký học ngay lập tức dù lớp đã khai giảng và học được gần một tháng.
+          </p>
+       <p>Kính chúc quý thầy cô và trung tâm sẽ càng ngày càng phát triển và gặt hái nhiều thành công hơn!
+       Em xin chân thành cảm ơn!!</p>
+     
+           <div class="student2">
+               <img src="../image/banner3.jpeg" alt="">
+               <h3>HUỲNH TRỌNG HẬU_LẬP TRÌNH VIÊN CHUYÊN NGHIỆP JAVA_CÔNG TY FSOFT</h3>
+           </div>`;
+   }
+
+
+}
+
+function change7(){
+
+
+    document.querySelector('.change7').onclick = function(){
+
+        document.querySelector(".render-new").innerHTML = `
+        <h2 style="text-align: center;">Học viên nói về trung tâm!</h2>
+        <div class="student">
+            <p>Mình đã tham gia khoá học Kiểm thử phần mềm tại đây. Nhờ sự giảng dạy rất nhiệt tình, sự tư vấn và hỗ trợ tìm việc làm sau khoá học rất tận tâm của các thầy cô ở trung tâm mà mình đã tìm được việc làm phù hợp. Xin chân thành cám ơn các thầy cô và mọi người ở trung tâm nhiều.</p>
+           
+            <div class="student2">
+                <img src="../image/banner3.jpeg" alt="">
+                <h3>ĐẶNG THỊ MI NA_TESTER_FSOFT</h3>
+            </div>`;
+    }
+
+    
+}
+
+change1();
+change2();
+change3();
+change4();
+change5();
+change6();
+change7();
+
+
+
+
+
+
+
+
+
+
+
+
